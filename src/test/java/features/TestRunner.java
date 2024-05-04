5ea1ecb3df27efeb55a6ca9cf36cb1e76186416f
ramkumars <ramkumars@ramkumars-air.lan>
2024-05-04 23:53:37 +0530

    sixth

--- src/test/java/features/TestRunner.java
+++ src/test/java/features/TestRunner.java
@@ -6,6 +6,26 @@ public class TestRunner {
 	
 	@Karate.Test
 	Karate getAPITest() {
-		return Karate.run("getapi").re
+		return Karate.run("getapi").relativeTo(getClass());
+	}
+	
+	@Karate.Test
+	Karate postAPITest() {
+		return Karate.run("postapi").relativeTo(getClass());
+	}
+	
+	@Karate.Test
+	Karate putAPITest() {
+		return Karate.run("putapi").relativeTo(getClass());
+	}
+	
+	@Karate.Test
+	Karate deleteAPITest() {
+		return Karate.run("deleteapi").relativeTo(getClass());
+	}
+	
+	@Karate.Test
+	Karate jsonAPITest() {
+		return Karate.run("json").relativeTo(getClass());
 	}
 }
